b1aee7cefea82f800bce5d7e41e65a854e7575d7
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-02-03 04:18:42 +0000

    Initial commit

---
+++ dia1/prueba1.py
@@ -0,0 +1,16 @@
+"""
+    prueba1
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 02/02/2023
+    Descripcion: operaciones aridmeticas dentro del print 
+"""
+
+variable1 = 8 #  variable para almacenar una cadena de caracter
+variable2 = 7 #  variable para almacenar una cadena de caracter
+print(variable1 + variable2)#  variable para almacenar una cadena de caracter
+
+print("{} + {} =".format(variable1, variable2),(variable1 + variable2))  #  variable para almacenar una cadena de caracter
+print("{} * {} =".format(variable1, variable2),(variable1 * variable2))  #  variable para almacenar una cadena de caracter
+print("{} / {} =".format(variable1, variable2),(variable1 / variable2))  #  variable para almacenar una cadena de caracter
+print("{} + {} =".format(variable1, variable2),(variable1 ** variable2))  #  variable para almacenar una cadena de caracter
+print("{} + {} =".format(variable1, variable2),(variable1 % variable2))  #  variable para almacenar una cadena de caracter
