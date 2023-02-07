e0537748663a02667d8c9821fadc2e2aa7d7a3eb
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-02-07 03:55:54 +0000

    Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces

---
+++ pruebas/dia4.py
@@ -0,0 +1,10 @@
+"""
+    dia3
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 05/02/2023
+    tema:Bucles
+    Descripcion: Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces
+"""
+palabra  = input("Introduzca una palabra: ")
+print((palabra + "\n") * int(10))
+
